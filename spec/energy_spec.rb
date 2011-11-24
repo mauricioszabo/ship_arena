90643fb5aeb54d3ebade6aa5a0aa3060d008b935
Maurício Szabo <mauricio.szabo@gmail.com>
2011-11-24 09:34:53 -0200

    Updated to new RSpec syntax.

--- spec/energy_spec.rb
+++ spec/energy_spec.rb
@@ -2,28 +2,25 @@ require_relative "helper"
 
 describe Energy do
   let(:screen) { MockScreen.new }
+  let(:energy) { Energy.new }
   class Energy; attr_reader :tank, :energy; end
 
-  before do
-    @energy = Energy.new
-  end
-
   it 'should have 100 points' do
-    @energy.level.should == 100
+    energy.level.should == 100
   end
 
   it 'should be able to get hit' do
-    @energy.hit(10)
-    @energy.level.should == 90
-    @energy.hit(100)
-    @energy.level.should == 0
+    energy.hit(10)
+    energy.level.should == 90
+    energy.hit(100)
+    energy.level.should == 0
   end
 
   it 'should draw correctly on the screen' do
-    @energy.draw_on screen, 0, 0
-    screen.should have_a(@energy.tank).on(0, 0)
-    screen.should have(100).objects_like(@energy.energy)
-    screen.should have_a(@energy.energy).on(4, 3)
-    screen.should have_a(@energy.energy).on(5, 3)
+    energy.draw_on screen, 0, 0
+    screen.should have_a(energy.tank).on(0, 0)
+    screen.should have(100).objects_like(energy.energy)
+    screen.should have_a(energy.energy).on(4, 3)
+    screen.should have_a(energy.energy).on(5, 3)
   end
 end
--- spec/sprite_spec.rb
+++ spec/sprite_spec.rb
@@ -1,13 +1,11 @@
 require_relative "helper"
 
 describe Sprite do
-  before do
-    @sprite = Sprite.new 'p1-1'
-  end
+  let(:sprite) { Sprite.new "p1-1" }
 
   it 'should be able to be drawed' do
-    @screen = mock("Screen")
-    @screen.should_receive(:draw).with(@sprite, 10, 20)
-    @sprite.draw_on(@screen, 10, 20)
+    screen = mock("Screen")
+    screen.should_receive(:draw).with(sprite, 10, 20)
+    sprite.draw_on(screen, 10, 20)
   end
 end
