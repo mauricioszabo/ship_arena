3878232461c98d0c91285fd660f66ef9a180b38b
Maurício Szabo <mauricio.szabo@gmail.com>
2012-02-24 20:25:17 -0200

    Simplified Scene code to update codes.

--- lib/scene.rb
+++ lib/scene.rb
@@ -1,5 +1,6 @@
 require_relative "sprite"
 require_relative "energy"
+require_relative "animable"
 require_relative "player"
 
 class Scene
@@ -8,10 +9,8 @@ class Scene
 
   def initialize(screen)
     @screen = screen
-    @p1_energy = Energy.new
-    @p2_energy = Energy.new
-    @p1 = Player.new :p1, 20, 300
-    @p2 = Player.new :p2, 780, 300
+    @p1 = Player.new :p1
+    @p2 = Player.new :p2
   end
   
   def update
@@ -31,28 +30,26 @@ class Scene
   end
 
   def update_codes
-    update_player_info @code1, @p1, @p2, @p1_energy, @p2_energy if @code1
-    update_player_info @code2, @p2, @p1, @p2_energy, @p1_energy if @code2
+    update_player_info @code1, @p1, @p2 if @code1
+    update_player_info @code2, @p2, @p1 if @code2
   end
   private :update_codes
 
-  def update_player_info(code, me, enemy, my_energy, other_energy)
+  def update_player_info(code, me, enemy)
     code.me ||= Code::ShipData.new
     code.enemy ||= Code::ShipData.new
     code.me.x = me.x.to_i
     code.me.y = me.y.to_i
-    code.me.energy = my_energy.level
+    code.me.energy = me.energy.level
     code.me.direction = Code::DIRECTION[me.current_animation + 1]
     code.enemy.x = enemy.x.to_i
     code.enemy.y = enemy.y.to_i
-    code.enemy.energy = other_energy.level
+    code.enemy.energy = enemy.energy.level
     code.enemy.direction = Code::DIRECTION[enemy.current_animation + 1]
   end
   private :update_player_info
   
   def draw
-    @p1_energy.draw_on @screen, 20, 0
-    @p2_energy.draw_on @screen, WIDTH-130, 0
     @p1.draw_on @screen
     @p2.draw_on @screen
   end
